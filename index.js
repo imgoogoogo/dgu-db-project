8e7ed9b1d6dd4357f05413c5320bddbff3374754
jnseo <coo2ng@naver.com>
2025-11-18 14:16:50 +0900

    1118

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = 3000;
 // ----------------------------------------------
 // 📌 공통 미들웨어
 // ----------------------------------------------
-app.use(cors());
+app.use(cors( { origin: '*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
